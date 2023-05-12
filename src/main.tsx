3dec3987d196e1c72aba66711e2fc42888e2a484
DjFIREED <porebskifilip@wp.pl>
2023-05-12 07:56:28 +0200

    redux configure setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { reducer as formReducer } from "redux-form";
+import { configureStore } from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({
+  form: formReducer,
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
